06e2ea9e807c97da95f1eca1428059a286045f35
yyhhenry <yyhhenry@foxmail.com>
2024-04-11 23:26:15 +0800

    set_default

--- xmake.lua
+++ xmake.lua
@@ -8,6 +8,7 @@ target("vscode-main")
     add_files("src/*.mpp", "src/*.cpp")
 
 target("main")
+    set_default(false)
     set_kind("binary")
     add_defines("ENABLE_CXX_MODULES")
     add_files("src/*.mpp", "src/*.cpp")
